6b0fed1754e9f0e883acafc3ed7c207f16a4ea25
kubkowski <jakub.opara@gmail.com>
2014-08-24 16:09:53 +0200

    Added login form

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -30,6 +30,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('swFrontApp').controller('LoginCtrl', function() {
+
+});
