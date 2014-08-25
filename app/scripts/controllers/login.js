f903b2f70dffc74cd69f3ce133581a146c9c16ad
kubkowski <jakub.opara@gmail.com>
2014-08-25 23:48:13 +0200

    Finished login form and validation

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,5 +1,12 @@
 'use strict';
 
-angular.module('swFrontApp').controller('LoginCtrl', function() {
-
+angular.module('swFrontApp').controller('LoginCtrl', function($scope) {
+	$scope.login = function() {
+		if ($scope.loginForm.$valid) {
+			console.log('sending request to server');
+		}
+		else {
+			$scope.loginForm.submitted = true;
+		}
+	}
 });
